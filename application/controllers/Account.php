d9778e6b96d4bdca6c7508c1a79f11f699c1038e
abalc <anca.balc@gmail.com>
2016-08-30 20:53:50 +0300

    Create account controller

---
+++ application/controllers/Account.php
@@ -0,0 +1,10 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Account extends CI_Controller {
+    
+    function create() {
+        echo "CREATE ACCOUNT";
+    }
+    
+}
