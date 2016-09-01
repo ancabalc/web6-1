331cac16826ff63e280ea5a9a52d6197ed66a80f
abalc <anca.balc@gmail.com>
2016-09-01 18:48:58 +0300

    Page layout and create view

--- application/controllers/Account.php
+++ application/controllers/Account.php
@@ -4,7 +4,10 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 class Account extends CI_Controller {
     
     function create() {
-        echo "CREATE ACCOUNT";
+        
+        $data['title'] = "CREATE ACCOUNT";
+        $data['pageContent'] = "createview.php";
+        $this->load->view('layout', $data);  
     }
     
 }
