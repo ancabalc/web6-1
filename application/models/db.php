e8c15ad1cb9a171fc18490f23c0199a96cc1ade5
Raluca <r4lu@yahoo.com>
2016-09-06 17:42:29 +0000

    create user model

---
+++ application/models/db.php
@@ -0,0 +1,28 @@
+<?php
+
+// Set DB configs
+    define("MySQL_USER", getenv('C9_USER'));
+    define("MySQL_PASSWORD", "");
+    define("MySQL_DB_NAME", "db");
+    define("MySQL_HOST", getenv('IP'));
+
+    class DB {
+        protected $dbh;
+        
+        function __construct() {
+            try {
+                // Set connection on class property
+                // include(APPPATH.'config/database'.EXT);
+                // $conn = mysql_connect($db['default']['hostname'], $db['default']['username'], $db['default']['password']);
+
+                // mysql_select_db($db['default']['database'], $conn);
+                
+                $this->dbh = new PDO('mysql:host=' . MySQL_HOST .';dbname=' . MySQL_DB_NAME, MySQL_USER, MySQL_PASSWORD);
+            } catch (PDOException $e) {
+                print "Error!:" . $e->getMessage() . "<br/>";
+            die();
+            }
+        }
+    }
+    
+?>
