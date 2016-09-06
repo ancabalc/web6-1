a69e7654dc0500e8f88e373ba29eac2457b9e94f
Marian Mates <Marian Mates>
2016-09-06 20:16:01 +0300

    Facebook login button

--- application/views/createview.php
+++ application/views/createview.php
@@ -1,1 +1,19 @@
 <h1>Create account page</h1>
+
+<!-- Facebook login button start -->
+<div id="fb-root"></div> 
+<script>
+    (function (d, s, id) {
+        var js, fjs = d.getElementsByTagName(s)[0];
+        if (d.getElementById(id))
+            return;
+        js = d.createElement(s);
+        js.id = id;
+        js.src = "//connect.facebook.net/en_US/sdk.js#xfbml=1&version=v2.7&appId=1285490414803865";
+        fjs.parentNode.insertBefore(js, fjs);
+    }(document, 'script', 'facebook-jssdk'));
+</script>
+
+<div class="fb-login-button" data-max-rows="1" data-size="large" data-show-faces="false" data-auto-logout-link="true"></div>
+
+<!-- Facebook login button finish -->
