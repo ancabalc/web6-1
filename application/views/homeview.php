ee6c3fc9590b4b2a5c780b91b01829a3141fd450
Marius Chesovan <marius.chesovan@yahoo.com>
2016-09-13 21:01:49 +0300

    commentu meu

---
+++ application/views/homeview.php
@@ -0,0 +1,28 @@
+<div >
+	<div class="left-half-container">
+
+<?php for ($i=1; $i<=6;$i++){ ?>
+
+		<div class="col-xs-6 col-md-3">
+			<a href="#" class="thumbnail"> <img
+				src="<?php echo base_url(); ?>assets/images/<?php echo $i; ?>.jpg"
+				alt="image">
+			</a>
+		</div>
+
+<?php }?>
+
+</div>
+	<div class="right-half-container">
+		<div class="media">
+			<div class="media-left">
+				<a href="#"> <img class="media-object" src="..." alt="...">
+				</a>
+			</div>
+			<div class="media-body">
+				<h4 class="media-heading">Media heading</h4>
+			</div>
+		</div>
+
+	</div>
+</div>
