48a20bda177289f5f37a927c6de2ab9c10f635ee
siitwebcluj <paul.brie@scoalainformala.ro>
2016-09-13 14:42:43 +0000

    add db tables

--- _db/db.sql
+++ _db/db.sql
@@ -2,10 +2,10 @@
 -- version 4.0.10deb1
 -- http://www.phpmyadmin.net
 --
--- Host: 127.0.0.1
--- Generation Time: Sep 06, 2016 at 04:10 PM
--- Server version: 5.5.49-0ubuntu0.14.04.1
--- PHP Version: 5.5.9-1ubuntu4.17
+-- Gazda: 127.0.0.1
+-- Timp de generare: 13 Sep 2016 la 14:41
+-- Versiune server: 5.5.50-0ubuntu0.14.04.1
+-- Versiune PHP: 5.5.9-1ubuntu4.19
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
@@ -17,14 +17,66 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8 */;
 
 --
--- Database: `db`
+-- Bază de date: `webapp_services`
 --
-CREATE DATABASE webapp_services;
+CREATE DATABASE IF NOT EXISTS `webapp_services` DEFAULT CHARACTER SET latin1 COLLATE latin1_swedish_ci;
+USE `webapp_services`;
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `users`
+-- Structura de tabel pentru tabelul `applications`
+--
+
+CREATE TABLE IF NOT EXISTS `applications` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `user_id` int(11) NOT NULL,
+  `title` varchar(255) NOT NULL,
+  `description` text NOT NULL,
+  `category_id` int(11) NOT NULL,
+  `creation_date` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `active` tinyint(4) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+
+-- --------------------------------------------------------
+
+--
+-- Structura de tabel pentru tabelul `categories`
+--
+
+CREATE TABLE IF NOT EXISTS `categories` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `title` varchar(255) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=3 ;
+
+--
+-- Salvarea datelor din tabel `categories`
+--
+
+INSERT INTO `categories` (`id`, `title`) VALUES
+(1, 'Meditatii'),
+(2, 'Curatenie & Menaj');
+
+-- --------------------------------------------------------
+
+--
+-- Structura de tabel pentru tabelul `offers`
+--
+
+CREATE TABLE IF NOT EXISTS `offers` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `user_id` int(11) NOT NULL,
+  `details` text NOT NULL,
+  `active` tinyint(4) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+
+-- --------------------------------------------------------
+
+--
+-- Structura de tabel pentru tabelul `users`
 --
 
 CREATE TABLE IF NOT EXISTS `users` (
