653e5f262731d5d0b820117ab8e4c2baa88b1c6a
siitwebcluj <paul.brie@scoalainformala.ro>
2016-09-22 16:37:09 +0000

    creare meniu + filtru applications

--- application/views/applicationview.php
+++ application/views/applicationview.php
@@ -53,9 +53,34 @@
 </form>
 <br><br>
 
-<div class="applications">
-	
-</div>
+
+   <div class="col-sm-3">
+      <form method="POST" id="filterform" onsubmit="return false">
+        <div class="checkbox">
+          <label>
+             <input type="checkbox" name="categories[]" value="1"/> Meditatii
+          </label>
+        </div>
+        <div class="checkbox">
+          <label>
+             <input type="checkbox" name="categories[]" value="2"/> Curatenie & Menaj
+          </label>
+        </div>
+        <select name="localitate" class="form-control">
+          <!--<option selected disabled hidden>Localitate</option>-->
+          <option selected value=''>Localitate</option>
+          <option value='1'>1</option>
+          <option value='2'>2</option>
+          <option value='3'>3</option>
+          <option value='4'>4</option>
+        </select><br>
+        <button type="submit" name="submit" class="btn searchapp btn-default">Search</button>
+      </form> 
+   </div><br><br> 
+    
+    <div class="col-sm-9 applications"></div>
+
+
 
 <script src="<?php echo base_url(); ?>assets/js/applications.js"
 	type="text/javascript"></script>
--- application/views/headerview.php
+++ application/views/headerview.php
@@ -1,6 +1,6 @@
 
 <header>
-	<nav class="navbar navbar-default">
+	<nav class="navbar navbar-inverse">
 		<div class="container-fluid">
 			<!-- Brand and toggle get grouped for better mobile display -->
 			<div class="navbar-header">
@@ -19,17 +19,9 @@
 				id="bs-example-navbar-collapse-1">
 
 				<ul class="nav navbar-nav navbar-right">
-					<li><a href="#">Link</a></li>
-					<li class="dropdown"><a href="#" class="dropdown-toggle"
-						data-toggle="dropdown" role="button" aria-haspopup="true"
-						aria-expanded="false">Dropdown <span class="caret"></span></a>
-						<ul class="dropdown-menu">
-							<li><a href="#">Action</a></li>
-							<li><a href="#">Another action</a></li>
-							<li><a href="#">Something else here</a></li>
-							<li role="separator" class="divider"></li>
-							<li><a href="#">Separated link</a></li>
-						</ul></li>
+					<li><a href="applications">Cereri</a></li>
+					<li><a href="create">Creeaza cont</a></li>
+					<li><a href="login">Intra in cont</a></li>
 				</ul>
 			</div>
 			<!-- /.navbar-collapse -->
